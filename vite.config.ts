35ce5f9d71e5f4670da030d0870da31ee9e98ca3
chrisratsabout <chrisratsabout@gmail.com>
2024-01-17 15:46:51 -0500

    remove extra objects from test

--- vite.config.ts
+++ vite.config.ts
@@ -9,30 +9,6 @@ export default defineConfig({
     globals: true,
     environment: 'happy-dom',
     setupFiles: ['src/setupTest.ts'],
-    environment: 'jsdom',
-    exclude: [...configDefaults.exclude, 'e2e/*'],
-    root: fileURLToPath(new URL('./', import.meta.url)),
-    coverage: {
-      exclude: [
-        '**/*.d.ts',
-        '**/.{eslint,mocha,prettier}rc.{?(c|m)js,yml}',
-        'src/router/**',
-        'src/config/**',
-        'src/types/**',
-        'src/main.tsx',
-        'src/routes/about.tsx',
-        'src/context/user-context.tsx'
-      ],
-      provider: 'v8',
-      reporter: ['text', 'json-summary', 'json', 'html'],
-      reportOnFailure: true,
-      thresholds: {
-        lines: 70,
-        functions: 70,
-        branches: 70,
-        statements: 70,
-      },
-    },
-    setupFiles: ['./vitest.setup.ts'],
+    
   },
 })
