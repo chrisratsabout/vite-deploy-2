cab9eaaa40014cfdffa7747f077c206f66a5a7bd
chrisratsabout <chrisratsabout@gmail.com>
2024-01-17 15:00:47 -0500

    add resolve

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 
@@ -9,5 +11,10 @@ export default defineConfig({
     globals: true,
     environment: 'happy-dom',
     setupFiles: ['src/setupTest.ts']
-  }
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
 })
