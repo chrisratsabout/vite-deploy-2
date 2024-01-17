281e02ac7228e492434f58c2c98e8935d6045b5f
chrisratsabout <chrisratsabout@gmail.com>
2024-01-17 15:45:37 -0500

    comment out all vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,31 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'happy-dom',
-    setupFiles: ['src/setupTest.ts']
+    setupFiles: ['src/setupTest.ts'],
+    environment: 'jsdom',
+    exclude: [...configDefaults.exclude, 'e2e/*'],
+    root: fileURLToPath(new URL('./', import.meta.url)),
+    coverage: {
+      exclude: [
+        '**/*.d.ts',
+        '**/.{eslint,mocha,prettier}rc.{?(c|m)js,yml}',
+        'src/router/**',
+        'src/config/**',
+        'src/types/**',
+        'src/main.tsx',
+        'src/routes/about.tsx',
+        'src/context/user-context.tsx'
+      ],
+      provider: 'v8',
+      reporter: ['text', 'json-summary', 'json', 'html'],
+      reportOnFailure: true,
+      thresholds: {
+        lines: 70,
+        functions: 70,
+        branches: 70,
+        statements: 70,
+      },
+    },
+    setupFiles: ['./vitest.setup.ts'],
   },
 })
