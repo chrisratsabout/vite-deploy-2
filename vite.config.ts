d74d7f7d3efe18b002134e92c8654f93e1b5eac5
chrisratsabout <chrisratsabout@gmail.com>
2024-01-18 13:01:01 -0500

    add tests and coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-deploy-2/'
+  base: '/vite-deploy-2/',
+  test: {
+    globals: true,
+    environment: 'happy-dom'
+  }
 })
